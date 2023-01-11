bc350b00407d6a07d0ab1f4dd23104f46036dad6
Vinnie Franco <vince@freshivore.net>
2023-01-10 19:33:02 -0600

    Packer now autoinstalls on new systems

--- lua/offbyjuan/packer.lua
+++ lua/offbyjuan/packer.lua
@@ -1,6 +1,17 @@
-vim.cmd.packadd("packer.nvim")
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
-return require("packer").startup(function(use)
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
   -- Packer can manage itself
   use "wbthomason/packer.nvim"
 
@@ -171,4 +182,10 @@ return require("packer").startup(function(use)
 
   -- AI fu
   use { "aduros/ai.vim" }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
